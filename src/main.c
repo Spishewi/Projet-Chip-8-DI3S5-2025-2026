a3e35950aceb98df1918f57b14320f994a62c959
Ethan333333 <ethannombella@gmail.com>
2025-12-04 12:23:25 +0100

    add function (RAM_print_instructions) that print instruction from ram

--- src/main.c
+++ src/main.c
@@ -8,11 +8,7 @@ int main(){
     struct RAM* ram = RAM_init();
     ROM_load_to_ram("./roms/1-chip8-logo.ch8", ram, 0x200);
 
-    uint16_t instruction = RAM_get_instruction(ram, 0x200);
-    printf("%s\n", instruction_as_str(instruction));
-
-    instruction = RAM_get_instruction(ram, 0x202);
-    printf("%s\n", instruction_as_str(instruction));
+    RAM_print_instructions(ram, 4093, 1);
     
     return 0;
 }
