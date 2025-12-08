ee3644d658cf79073ef56955ce7cceb3caacc64a
Ethan333333 <ethannombella@gmail.com>
2025-12-08 05:43:34 +0100

    trying to do function (CPU_init) and CPU_destroy)

--- src/system/cpu.c
+++ src/system/cpu.c
@@ -1,13 +1,28 @@
 #include <stdint.h>
 #include <display/display.h>
 
+#include "system/cpu.h"
 #include "system/ram.h"
 
 /*allocate and initialize a CPU*/
-struct CPU* CPU_init(struct RAM* ram, struct Display* display){}
+struct CPU* CPU_init(struct RAM* ram, struct Display* display){
+    /*allocate the CPU array*/
+    /*uses malloc to allocate the memory*/
+    struct CPU* cpu = malloc(sizeof(struct CPU));
+
+    /*if the allocation havn't worked, return NULL*/
+    if(!cpu){
+        fprintf(stderr, "[ERROR] : CPU cannot be allocated.");
+        exit(1);
+    }
+
+    return cpu;
+}
 
 /*free the CPU*/
-void CPU_destroy(struct CPU* cpu){}
+void CPU_destroy(struct CPU* cpu){
+    free(cpu);
+}
 
 /*Do a fetch-decode-execute cycle*/
 int CPU_FDE(struct CPU* cpu){}
