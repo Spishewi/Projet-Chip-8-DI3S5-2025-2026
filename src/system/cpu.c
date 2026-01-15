e72f96395a0e86a123bdb6b803edb2b601293cfa
Ethan333333 <ethannombella@gmail.com>
2026-01-15 11:32:45 +0100

    :memo: README

--- src/system/cpu.c
+++ src/system/cpu.c
@@ -1,3 +1,22 @@
+/*-------------------------------------------------------------------------*
+ | Copyright (C) 2026 Aurèle AUMONT--VESNIER and Ethan NOMBELLA.           |
+ |                                                                         |
+ | This file is part of chip-8-s5, yet another CHIP 8 emulator.            |
+ |                                                                         |
+ | chip-8-s5 is free software; you can redistribute it and/or modify       |
+ | it under the terms of the GNU General Public License as published by    |
+ | the Free Software Foundation; either version 3 of the License,          |
+ | or (at your option) any later version.                                  |
+ |                                                                         |
+ | chip-8-s5 is distributed in the hope that it will be useful,            |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
+ | GNU General Public License for more details.                            |
+ |                                                                         |
+ | You should have received a copy of the GNU General Public License       |
+ | along with this program. If not, see <http://www.gnu.org/licenses/>.    |
+ *-------------------------------------------------------------------------*/
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
